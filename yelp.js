c073394b4b4d31c47ad54ddcffea31932e8d5e19
daogen <daoyeyemi@aol.com>
2020-09-04 15:43:36 -0700

    added queryURL and added necessary variables

--- yelp.js
+++ yelp.js
@@ -1,9 +1,17 @@
 var apiKey = "Bearer huZM0jW7oxfJk_QU1FrFl1ZnuU8y3B8yAFAlAn7RKVr8YIRsHqibw2RnwD19sD2vXCCTTOUNJfwV5VMTEPSy7IecJkDUzzHDMj5fWehQ7wN8GSj2VPe_16ZdbCIrX3Yx"
     
-    var address = "Gardena";
+var category = "";
     
-    var category = "tacos";
+var city = "";
+    
+var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?limit=30&location=" + city + "&locale=it_IT&categories=" + category + "&term=restaurant";
+    
+// var longitude = "";
 
+// var lattitude = "";
+
+// var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?limit=10&latitude=" + latitude + "&longitude=" + longitude + "&locale=it_IT&categories=" + category + "&term=restaurant";
+    
 function runYelp(){
 
     $.ajax({
