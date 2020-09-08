54aadcd855f8c65f3dd33fece24bad53aad7f480
Tristan Pacba <tpacba@live.com>
2020-09-08 01:50:01 -0700

    configured get routes all the way till creategroup and joingroup, left password as null for config.json

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../public/landing.html"));
   });
 
   app.get("/login", (req, res) => {
@@ -22,9 +22,25 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
+  app.get("/signup", (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, "../public/members.html"));
+  });
+
+  app.get("/creategroup", isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/creategroup.html"));
+  });
+
+  app.get("/joingroup", isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/joingroup.html"));
   });
 };
