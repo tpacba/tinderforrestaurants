68ab08d53aa66e595fc0bce124c777d67c9f4c21
AileenWang <ainiwang14@gmail.com>
2020-09-09 11:29:47 -0700

    check/ex img functionality to move carousel

--- public/js/restaurant.js
+++ public/js/restaurant.js
@@ -1,9 +1,18 @@
 $(document).ready(()=> {
     //api calls and dom manipulation happens
-    const code = window.location.href.split("/results/").slice(-1)[0];
-    $.get(`/api/results/${code}`)
-    .then(data => {
-        //put data in carousel or something...
-        console.log(data)
+    // const code = window.location.href.split("/results/").slice(-1)[0];
+    // $.get(`/api/results/${code}`)
+    // .then(data => {
+    //     //put data in carousel or something...
+    //     console.log(data)
+    // })
+    $('.carousel').carousel('pause');
+    $("#check-1").click(function() {
+        $('.carousel').carousel('next')
     })
+    $("#ex-1").click(function() {
+        $('.carousel').carousel('next')
+    })
+
+    console.log("hello")
 })
